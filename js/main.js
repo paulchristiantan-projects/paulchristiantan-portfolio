18036118c27d282a439a3383c1da13d37632e8d9
Paul Christian Tan <paulchristiantan.projects@gmail.com>
2023-05-08 18:51:43 +0800

    Added theme feature

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//for Themes (Light and Dark mode)
+const body = document.querySelector("body"),
+toggle = document.querySelector(".toggle");
+
+toggle.addEventListener("click", () => {
+    body.classList.toggle("dark");
+});
+toggle.addEventListener("click", () => toggle.classList.toggle("active"));
