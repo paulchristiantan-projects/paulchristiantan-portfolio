8d8b5680c6e3ec374d968222fedca9da61b8dc13
Paul Christian Tan <paulchristiantan.projects@gmail.com>
2023-05-21 17:59:24 +0800

    Add Front page info

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 //for Themes (Light and Dark mode)
-const body = document.querySelector("body"),
+const body = document.querySelector("body");
+const textPosition = document.querySelector(".position");
 toggle = document.querySelector(".toggle");
 
 toggle.addEventListener("click", () => {
     body.classList.toggle("dark");
+
 });
 toggle.addEventListener("click", () => toggle.classList.toggle("active"));
+
+// for Position 
+const text = document.querySelector(".position");
+const textLoad = () => {
+    setTimeout(()=> {
+        text.textContent = "Front-End Developer";
+    }, 0);
+    setTimeout(()=> {
+        text.textContent = "Software Engineer";
+    }, 4000);
+    setTimeout(()=> {
+        text.textContent = "Content Creator";
+    }, 8000);
+}
+
+textLoad();
+setInterval(textLoad, 12000);
