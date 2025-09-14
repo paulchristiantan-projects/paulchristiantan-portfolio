407a606dc9685c44737d5ad951b3505a4c9d41f8
Paul Christian Tan <paulchristiantan.projects@gmail.com>
2025-09-14 20:31:30 +0800

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        © 2025 Paul Christian Tan. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
